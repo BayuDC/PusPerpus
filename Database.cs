93c5ecac12d7b4ea011d7efee73a08b89a2bb7da
BayuDC <dev.bayudc@gmail.com>
2022-02-25 13:24:06 +0700

    Add database models

---
+++ Database.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace PusPerpus {
+    public partial class Database : DbContext {
+        public Database() {
+        }
+
+        public Database(DbContextOptions<Database> options)
+            : base(options) {
+        }
+
+        public virtual DbSet<Book> Books { get; set; }
+        public virtual DbSet<Genre> Genres { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            if (!optionsBuilder.IsConfigured) {
+                optionsBuilder.UseSqlServer("Server=SHRYK-WIN;Database=PusPerpus;Trusted_Connection=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Book>(entity => {
+                entity.ToTable("books");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Author)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("author");
+
+                entity.Property(e => e.GenreId).HasColumnName("genre_id");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .IsUnicode(false)
+                    .HasColumnName("name");
+
+                entity.HasOne(d => d.Genre)
+                    .WithMany(p => p.Books)
+                    .HasForeignKey(d => d.GenreId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_books_genres");
+            });
+
+            modelBuilder.Entity<Genre>(entity => {
+                entity.ToTable("genres");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("name");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
